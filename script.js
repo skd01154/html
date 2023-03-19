6a32ce7d5a23e83b98d20ec486df0e64b4fb38ea
skd01154 <128318966+skd01154@users.noreply.github.com>
2023-03-20 08:27:17 +0900

    sss

--- script.js
+++ script.js
@@ -1,66 +1,68 @@
-const board = document.getElementById('board');
-const numbers = [];
+const boardSize = 7; // 보드의 크기
+const boardContainer = document.querySelector('#board'); // 보드가 들어갈 div 요소
+const board = []; // 보드 배열
 
-for (let i = 1; i <= 49; i++) {
-  const number = document.createElement('div');
-  number.classList.add('number');
-  number.setAttribute('id', i);
-  number.textContent = i;
-  board.appendChild(number);
-  numbers.push(number);
-}
+const elements = ['sss', 'ss', 's', 'a', 'b', 'c']; // 보드에 들어갈 요소들
 
-const sssButton = document.createElement('button');
-sssButton.textContent = 'sss';
-document.body.appendChild(sssButton);
-
-let selectedNumber = null;
-
-function handleNumberClick(event) {
-  if (selectedNumber) {
-    selectedNumber.classList.remove('selected');
+// 'c'가 연속되지 않는 배열 생성
+let arr = new Array(elements.length).fill(0);
+arr = arr.map((_, index) => index);
+let lastIndex = -1;
+while (true) {
+  const shuffledArr = shuffleArray(arr);
+  if (lastIndex === -1 || shuffledArr[0] !== lastIndex + 1) {
+    board.push(...shuffledArr);
+    lastIndex = shuffledArr[shuffledArr.length - 1];
   }
-  selectedNumber = event.currentTarget;
-  selectedNumber.classList.add('selected');
+  if (board.length >= boardSize ** 2) break;
 }
 
-function handleSssButtonClick() {
-  selectedNumber = null;
-  for (let i = 0; i < numbers.length; i++) {
-    const prevNumber = numbers[i - 1];
-    const currentNumber = numbers[i];
-    const nextNumber = numbers[i + 1];
-    if (
-      currentNumber.classList.contains('c') &&
-      (!prevNumber || !prevNumber.classList.contains('c')) &&
-      (!nextNumber || !nextNumber.classList.contains('c'))
-    ) {
-      currentNumber.classList.remove('c');
-      const randomNumber = Math.floor(Math.random() * 6);
-      switch (randomNumber) {
-        case 0:
-          currentNumber.classList.add('sss');
-          break;
-        case 1:
-          currentNumber.classList.add('ss');
-          break;
-        case 2:
-        case 3:
-        case 4:
-          currentNumber.classList.add('s');
-          break;
-        case 5:
-          currentNumber.classList.add('a');
-          break;
+// 숫자판 생성
+for (let i = 0; i < boardSize; i++) {
+  const row = document.createElement('div');
+  row.classList.add('row');
+  for (let j = 0; j < boardSize; j++) {
+    const index = i * boardSize + j;
+    const cell = document.createElement('div');
+    cell.classList.add('cell');
+    cell.innerText = board[index] + 1;
+    cell.dataset.value = elements[board[index]];
+    cell.addEventListener('mouseover', () => {
+      cell.innerText = '선택';
+    });
+    cell.addEventListener('mouseout', () => {
+      cell.innerText = board[index] + 1;
+    });
+    cell.addEventListener('click', () => {
+      cell.classList.add('selected');
+      cell.innerText = cell.dataset.value;
+      const sssCells = document.querySelectorAll('.cell[data-value="sss"]');
+      if (sssCells.length === 0) {
+        setTimeout(() => {
+          resetBoard();
+        }, 1000);
       }
-    }
+    });
+    row.appendChild(cell);
   }
-  result.textContent = '';
+  boardContainer.appendChild(row);
 }
 
-const result = document.createElement('div');
-result.setAttribute('id', 'result');
-document.body.appendChild(result);
+// 보드 재설정
+function resetBoard() {
+  const selectedCells = document.querySelectorAll('.selected');
+  selectedCells.forEach((cell) => {
+    cell.classList.remove('selected');
+    cell.innerText = board[cell.parentNode.dataset.row * boardSize + cell.dataset.col] + 1;
+  });
+}
 
-board.addEventListener('click', handleNumberClick);
-sssButton.addEventListener('click', handleSssButtonClick);
+// 배열 랜덤 셔플
+function shuffleArray(array) {
+  const newArray = [...array];
+  for (let i = newArray.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [newArray[i], newArray[j]] = [newArray[j], newArray[i]];
+  }
+  return newArray;
+}
