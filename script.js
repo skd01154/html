62cc903cd549583e6023de87025b4efb483cc178
skd01154 <128318966+skd01154@users.noreply.github.com>
2023-03-20 06:44:31 +0900

    ss

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-// Create an array of characters
 const arr = Array(1).fill('sss')
             .concat(Array(2).fill('ss'))
             .concat(Array(6).fill('s'))
@@ -6,7 +5,6 @@ const arr = Array(1).fill('sss')
             .concat(Array(18).fill('b'))
             .concat(Array(12).fill('c'));
 
-// Randomly shuffle the array without two consecutive 'c'
 function shuffleArray(arr) {
   let prev = '';
   for (let i = arr.length - 1; i > 0; i--) {
@@ -20,30 +18,53 @@ function shuffleArray(arr) {
   }
 }
 
+// Shuffle the array
 shuffleArray(arr);
 
-// Create the number board
-const board = document.getElementById('board');
+const container = document.querySelector('.container');
+
+// Fill the cells with the corresponding element in the array
 for (let i = 0; i < 7; i++) {
-  let row = board.insertRow(i);
   for (let j = 0; j < 7; j++) {
-    let cell = row.insertCell(j);
-    cell.innerHTML = arr[i * 7 + j];
-    cell.addEventListener('mouseover', function() {
-      cell.classList.add('hover');
-      cell.innerHTML = '선택';
-    });
-    cell.addEventListener('mouseout', function() {
-      cell.classList.remove('hover');
-      cell.innerHTML = arr[i * 7 + j];
-    });
-    cell.addEventListener('click', function() {
-      cell.classList.toggle('selected');
-      if (cell.classList.contains('selected')) {
-        cell.innerHTML = arr[i * 7 + j];
-      } else {
-        cell.innerHTML = '';
-      }
-    });
+    const cell = document.createElement('div');
+    cell.classList.add('cell');
+    cell.dataset.value = arr[i * 7 + j];
+    cell.textContent = i * 7 + j + 1;
+    container.appendChild(cell);
   }
 }
+
+// Add event listener to the cells
+const cells = document.querySelectorAll('.cell');
+let selected = null;
+cells.forEach(cell => {
+  cell.addEventListener('mouseenter', () => {
+    if (!cell.classList.contains('selected')) {
+      cell.style.backgroundColor = 'gray';
+    }
+  });
+  cell.addEventListener('mouseleave', () => {
+    if (!cell.classList.contains('selected')) {
+      cell.style.backgroundColor = '';
+    }
+  });
+  cell.addEventListener('click', () => {
+    if (cell.dataset.value === 'sss') {
+      shuffleArray(arr);
+      cells.forEach(cell => {
+        cell.dataset.value = arr[parseInt(cell.textContent) - 1];
+        cell.classList.remove('selected');
+        cell.style.backgroundColor = '';
+      });
+    } else {
+      cell.classList.toggle('selected');
+      if (cell.classList.contains('selected')) {
+        cell.style.backgroundColor = 'yellow';
+        selected = cell;
+      } else {
+        cell.style.backgroundColor = '';
+        selected = null;
+      }
+    }
+  });
+});
