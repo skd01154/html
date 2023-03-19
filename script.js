1f579a55684292ded33adfbcc723eccacb725821
skd01154 <128318966+skd01154@users.noreply.github.com>
2023-03-20 08:54:46 +0900

    sss

--- script.js
+++ script.js
@@ -1,74 +1,72 @@
-// 상수 선언
-const size = 7; // 숫자판 크기
-const board = document.querySelector('#board'); // 숫자판 영역
-const elements = ['sss', 'ss', 's', 'a', 'b', 'c']; // 원소들
-const notContinuous = [1, 3, 5, 7, 9, 12, 14, 18, 22, 25, 27, 31, 33, 37, 39, 43, 45, 47]; // c 원소가 연속되지 않는 숫자판 인덱스
-let selectedElements = []; // 선택된 원소들의 인덱스
+let numArray = [];
+let num = 1;
+for (let i = 0; i < 7; i++) {
+  let row = [];
+  for (let j = 0; j < 7; j++) {
+    row.push(num);
+    num++;
+  }
+  numArray.push(row);
+}
 
-// 함수 선언
-function createBoard() {
-  // 숫자판 생성
-  for (let i = 0; i < size; i++) {
-    const row = document.createElement('div');
-    row.classList.add('row');
-    board.appendChild(row);
+const arr = Array(1).fill('sss')
+  .concat(Array(2).fill('ss'))
+  .concat(Array(6).fill('s'))
+  .concat(Array(10).fill('a'))
+  .concat(Array(18).fill('b'))
+  .concat(Array(12).fill('c'));
 
-    for (let j = 0; j < size; j++) {
-      const cell = document.createElement('div');
-      cell.classList.add('cell');
-      const index = i * size + j + 1;
-      if (notContinuous.includes(index)) {
-        cell.classList.add('not-continuous');
-      }
-      const elementIndex = Math.floor(Math.random() * elements.length);
-      const element = elements[elementIndex];
-      cell.setAttribute('data-element', element);
-      cell.setAttribute('data-index', index);
-      cell.addEventListener('mouseover', handleCellMouseOver);
-      cell.addEventListener('mouseout', handleCellMouseOut);
-      cell.addEventListener('click', handleCellClick);
-      row.appendChild(cell);
+function shuffleArray(arr) {
+  let prev = '';
+  for (let i = arr.length - 1; i > 0; i--) {
+    if (arr[i] === 'c' && prev === 'c') {
+      [arr[i], arr[i - 1]] = [arr[i - 1], arr[i]];
+    } else {
+      let j = Math.floor(Math.random() * (i + 1));
+      [arr[i], arr[j]] = [arr[j], arr[i]];
+      prev = arr[i];
     }
   }
 }
+shuffleArray(arr);
 
-function handleCellMouseOver(event) {
-  event.target.textContent = '선택';
+const obj = {
+  'sss': [],
+  'ss': [],
+  's': [],
+  'a': [],
+  'b': [],
+  'c': []
+};
+let index = 0;
+for (const prop in obj) {
+  const len = arr.filter(elem => elem === prop).length;
+  obj[prop] = numArray.slice(index, index + len).flat();
+  index += len;
 }
 
-function handleCellMouseOut(event) {
-  event.target.textContent = '';
-}
-
-function handleCellClick(event) {
-  const clickedIndex = parseInt(event.target.dataset.index);
-  if (selectedElements.includes(clickedIndex)) {
-    return;
+const boardDiv = document.getElementById('board');
+for (let i = 0; i < 7; i++) {
+  for (let j = 0; j < 7; j++) {
+    const cellDiv = document.createElement('div');
+    cellDiv.classList.add('cell');
+    const numSpan = document.createElement('span');
+    numSpan.innerText = numArray[i][j];
+    cellDiv.appendChild(numSpan);
+    const hiddenSpan = document.createElement('span');
+    hiddenSpan.innerText = obj[arr[numArray[i][j] - 1]];
+    hiddenSpan.classList.add('hidden');
+    cellDiv.appendChild(hiddenSpan);
+    cellDiv.addEventListener('mouseover', function() {
+      hiddenSpan.innerText = '선택';
+    });
+    cellDiv.addEventListener('mouseout', function() {
+      hiddenSpan.innerText = '';
+    });
+    cellDiv.addEventListener('click', function() {
+      numSpan.classList.add('hidden');
+      hiddenSpan.classList.remove('hidden');
+    });
+    boardDiv.appendChild(cellDiv);
   }
-  selectedElements.push(clickedIndex);
-  const clickedElement = event.target.dataset.element;
-  const cells = document.querySelectorAll('.cell');
-  cells.forEach(cell => {
-    if (cell.dataset.element === clickedElement) {
-      cell.classList.add('selected');
-    }
-  });
-  event.target.style.visibility = 'hidden';
-  if (clickedElement === 'sss') {
-    setTimeout(() => {
-      resetBoard();
-    }, 1000);
-  }
 }
-
-function resetBoard() {
-  selectedElements = [];
-  const cells = document.querySelectorAll('.cell');
-  cells.forEach(cell => {
-    cell.style.visibility = 'visible';
-    cell.classList.remove('selected');
-  });
-}
-
-// 실행
-createBoard();
