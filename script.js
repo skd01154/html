465ba4105cb1a6a58cd980b69cc9a220c330bd3f
skd01154 <128318966+skd01154@users.noreply.github.com>
2023-03-20 08:38:13 +0900

    ddd

--- script.js
+++ script.js
@@ -1,68 +1,74 @@
-const boardSize = 7; // 보드의 크기
-const boardContainer = document.querySelector('#board'); // 보드가 들어갈 div 요소
-const board = []; // 보드 배열
+// 상수 선언
+const size = 7; // 숫자판 크기
+const board = document.querySelector('#board'); // 숫자판 영역
+const elements = ['sss', 'ss', 's', 'a', 'b', 'c']; // 원소들
+const notContinuous = [1, 3, 5, 7, 9, 12, 14, 18, 22, 25, 27, 31, 33, 37, 39, 43, 45, 47]; // c 원소가 연속되지 않는 숫자판 인덱스
+let selectedElements = []; // 선택된 원소들의 인덱스
 
-const elements = ['sss', 'ss', 's', 'a', 'b', 'c']; // 보드에 들어갈 요소들
+// 함수 선언
+function createBoard() {
+  // 숫자판 생성
+  for (let i = 0; i < size; i++) {
+    const row = document.createElement('div');
+    row.classList.add('row');
+    board.appendChild(row);
 
-// 'c'가 연속되지 않는 배열 생성
-let arr = new Array(elements.length).fill(0);
-arr = arr.map((_, index) => index);
-let lastIndex = -1;
-while (true) {
-  const shuffledArr = shuffleArray(arr);
-  if (lastIndex === -1 || shuffledArr[0] !== lastIndex + 1) {
-    board.push(...shuffledArr);
-    lastIndex = shuffledArr[shuffledArr.length - 1];
-  }
-  if (board.length >= boardSize ** 2) break;
-}
-
-// 숫자판 생성
-for (let i = 0; i < boardSize; i++) {
-  const row = document.createElement('div');
-  row.classList.add('row');
-  for (let j = 0; j < boardSize; j++) {
-    const index = i * boardSize + j;
-    const cell = document.createElement('div');
-    cell.classList.add('cell');
-    cell.innerText = board[index] + 1;
-    cell.dataset.value = elements[board[index]];
-    cell.addEventListener('mouseover', () => {
-      cell.innerText = '선택';
-    });
-    cell.addEventListener('mouseout', () => {
-      cell.innerText = board[index] + 1;
-    });
-    cell.addEventListener('click', () => {
-      cell.classList.add('selected');
-      cell.innerText = cell.dataset.value;
-      const sssCells = document.querySelectorAll('.cell[data-value="sss"]');
-      if (sssCells.length === 0) {
-        setTimeout(() => {
-          resetBoard();
-        }, 1000);
+    for (let j = 0; j < size; j++) {
+      const cell = document.createElement('div');
+      cell.classList.add('cell');
+      const index = i * size + j + 1;
+      if (notContinuous.includes(index)) {
+        cell.classList.add('not-continuous');
       }
-    });
-    row.appendChild(cell);
+      const elementIndex = Math.floor(Math.random() * elements.length);
+      const element = elements[elementIndex];
+      cell.setAttribute('data-element', element);
+      cell.setAttribute('data-index', index);
+      cell.addEventListener('mouseover', handleCellMouseOver);
+      cell.addEventListener('mouseout', handleCellMouseOut);
+      cell.addEventListener('click', handleCellClick);
+      row.appendChild(cell);
+    }
   }
-  boardContainer.appendChild(row);
 }
 
-// 보드 재설정
+function handleCellMouseOver(event) {
+  event.target.textContent = '선택';
+}
+
+function handleCellMouseOut(event) {
+  event.target.textContent = '';
+}
+
+function handleCellClick(event) {
+  const clickedIndex = parseInt(event.target.dataset.index);
+  if (selectedElements.includes(clickedIndex)) {
+    return;
+  }
+  selectedElements.push(clickedIndex);
+  const clickedElement = event.target.dataset.element;
+  const cells = document.querySelectorAll('.cell');
+  cells.forEach(cell => {
+    if (cell.dataset.element === clickedElement) {
+      cell.classList.add('selected');
+    }
+  });
+  event.target.style.visibility = 'hidden';
+  if (clickedElement === 'sss') {
+    setTimeout(() => {
+      resetBoard();
+    }, 1000);
+  }
+}
+
 function resetBoard() {
-  const selectedCells = document.querySelectorAll('.selected');
-  selectedCells.forEach((cell) => {
+  selectedElements = [];
+  const cells = document.querySelectorAll('.cell');
+  cells.forEach(cell => {
+    cell.style.visibility = 'visible';
     cell.classList.remove('selected');
-    cell.innerText = board[cell.parentNode.dataset.row * boardSize + cell.dataset.col] + 1;
   });
 }
 
-// 배열 랜덤 셔플
-function shuffleArray(array) {
-  const newArray = [...array];
-  for (let i = newArray.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [newArray[i], newArray[j]] = [newArray[j], newArray[i]];
-  }
-  return newArray;
-}
+// 실행
+createBoard();
