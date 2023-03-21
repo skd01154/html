4ea3b49e1b53f4655b834505431c7943d110b8ae
skd01154 <128318966+skd01154@users.noreply.github.com>
2023-03-21 15:17:06 +0900

    dd

--- script.js
+++ script.js
@@ -45,18 +45,30 @@ const resultDiv = document.createElement("div");
 resultDiv.innerHTML = `sss=${xxx[0]} ss=${xxx[1]} s=${xxx[2]} a=${xxx[3]} b=${xxx[4]} c=${xxx[5]}`;
 resultContainer.appendChild(resultDiv);
 
+const numbers = [];
+let count = 1;
+
+for (let i = 0; i < 7; i++) {
+  const row = [];
+  for (let j = 0; j < 7; j++) {
+    row.push(count++);
+  }
+  numbers.push(row);
+}
+
 for (let i = 0; i < row; i++) {
   for (let j = 0; j < col; j++) {
     const box = document.createElement("span");
     box.className = "box";
     box.dataset.row = i;
     box.dataset.col = j;
+    box.innerHTML = numbers[i][j];
     box.addEventListener('click', function() {
       const value = xx[i * col + j];
       console.log(value);
       box.innerHTML = value;
       if(value=="sss"){
-        console.log("축하합니다.");
+        xxx[1]--;
       } else if (value=="ss"){
         xxx[1]--;
       } else if (value=="s"){
@@ -74,3 +86,37 @@ for (let i = 0; i < row; i++) {
   }
   boxContainer.appendChild(document.createElement("br"));
 }
+
+const resetButton = document.getElementById("reset-button");
+resetButton.addEventListener("click", function() {
+  // xxx[0]이 0일 때 또는 다시하기 버튼을 눌렀을 때 초기화
+  if (xxx[0] === 0 || this.id === "reset-button") {
+    xx = [];
+    let x = 0;
+    while (x < 5) {
+      xx = [...xx, ...Array(ac[x]).fill(a[x])];
+      x = x + 1;
+    }
+    shuffle(xx);
+    xx.unshift(0);
+    xx.push(0);
+    let i = 0;
+    while (i < 12) {
+      const randomIndex = Math.floor(Math.random() * (xx.length - 2)) + 1;
+      if (xx[randomIndex] !== "c" && xx[randomIndex - 1] !== "c" && xx[randomIndex + 1] !== "c") {
+        xx.splice(randomIndex, 0, "c");
+        i++;
+      }
+    }
+    xx.pop();
+    xx.shift();
+  }
+  // 게임 보드 초기화
+  const boxes = document.querySelectorAll(".box");
+  for (let i = 0; i < boxes.length; i++) {
+    boxes[i].innerHTML = numbers[Math.floor(i / col)][i % col];
+  }
+  // 결과창 초기화
+  xxx=[1,2,6,10,18,12];
+  resultDiv.innerHTML = `sss=${xxx[0]} ss=${xxx[1]} s=${xxx[2]} a=${xxx[3]} b=${xxx[4]} c=${xxx[5]}`;
+});
