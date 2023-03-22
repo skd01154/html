f6e1609b6602797a42a9705fe7b0789c569b8534
skd01154 <128318966+skd01154@users.noreply.github.com>
2023-03-22 11:20:56 +0900

    dd

--- script.js
+++ script.js
@@ -98,7 +98,7 @@ for (let i = 0; i < row; i++) {
           }
           const resultDiv = document.createElement("div");
           resultDiv.className = "resultDiv";
-          resultDiv.innerHTML = `sss: ${xxx[0]}${" ".repeat(100)}ss: ${xxx[1]}${" ".repeat(100)}s: ${xxx[2]}${" ".repeat(100)}a: ${xxx[3]}${" ".repeat(100)}b: ${xxx[4]}${" ".repeat(100)}c: ${xxx[5]}`;
+          resultDiv.innerHTML = `sss: ${xxx[0]}${" ".repeat(50)}ss: ${xxx[1]}${" ".repeat(50)}s: ${xxx[2]}${" ".repeat(50)}a: ${xxx[3]}${" ".repeat(50)}b: ${xxx[4]}${" ".repeat(50)}c: ${xxx[5]}`;
           resultContainer.innerHTML = "";
           resultContainer.appendChild(resultDiv);
           
