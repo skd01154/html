70b6ddf54047598aa7b1218fb223d70163980e28
skd01154 <128318966+skd01154@users.noreply.github.com>
2023-03-22 09:48:53 +0900

    dddd

--- script.js
+++ script.js
@@ -41,7 +41,7 @@ const boxContainer = document.getElementById("box-container");
 const row = 7;
 const col = 7;
 
-let xxx=[1,2,6,10,18,12];
+var xxx=[1,2,6,10,18,12];
 const resultDiv = document.createElement("div");
 resultDiv.innerHTML = `sss: ${xxx[0]}`+`     `+`ss: ${xxx[1]}`+`     `+`s: ${xxx[2]}`+`     `+`a: ${xxx[3]}`+`     `+`b: ${xxx[4]}`+`     `+`c: ${xxx[5]}`;
 resultContainer.appendChild(resultDiv);
@@ -66,42 +66,37 @@ for (let i = 0; i < row; i++) {
     box.dataset.row = i;
     box.dataset.col = j;
     box.innerHTML = numbers[i][j];
-    function updateXXXArray(xxx, i, j, value) {
-      switch(value) {
-        case 'sss':
-          xxx[0]--;
-          break;
-        case 'ss':
-          xxx[1]--;
-          break;
-        case 's':
-          xxx[2]--;
-          break;
-        case 'a':
-          xxx[3]--;
-          break;
-        case 'b':
-          xxx[4]--;
-          break;
-        case 'c':
-          xxx[5]--;
-          break;
-      }
-    }
-
-    
     box.addEventListener('click', function() {
-      const value = xx[i * col + j];
-      console.log(value);
-      if (box.innerHTML !== value) {
-        updateXXXArray(xxx, i, j, value);
+      if (box.innerHTML == numbers[i][j]) {
+        const value = xx[i * col + j];
+        console.log(value);
         box.innerHTML = value;
+        switch(value) {
+          case 'sss':
+            xxx[0]--;
+            break;
+          case 'ss':
+            xxx[1]--;
+            break;
+          case 's':
+            xxx[2]--;
+            break;
+          case 'a':
+            xxx[3]--;
+            break;
+          case 'b':
+            xxx[4]--;
+            break;
+          case 'c':
+            xxx[5]--;
+            break;
+          }
       }
     });
     boxContainer.appendChild(box);
   }
   boxContainer.appendChild(document.createElement("br"));
-}
 
+}
 
 const resetButton = document.getElementById("reset-button");
